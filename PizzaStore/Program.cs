d32753748272e4949267ae94769f9073b4af52da
Gamuchirai Chaita <69142703+abgc195@users.noreply.github.com>
2023-10-12 13:55:32 +0000

    Pending changes exported from your codespace

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -4,29 +4,52 @@ using Microsoft.OpenApi.Models;
 using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("pizzas") ?? "Data Source=pizzas.db";
+var relativePath = "pizzas.db";
+var parentdir = Path.GetDirectoryName(Environment.CurrentDirectory);
+string absolutePath = Path.Combine(parentdir, "PizzaStore", relativePath);
+Console.WriteLine("{0} {1}", parentdir, absolutePath);
+var connectionString = builder.Configuration.GetConnectionString("pizzas") ?? string.Format("Data Source={0}", absolutePath);
 builder.Services.AddDbContext<PizzaDb>(options => options.UseSqlite(connectionString));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
   c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pizzas API", Description = "Pizza pizza", Version = "v1" });
 });
+// 1) define a unique string
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// 2) define allowed domains, in this case "http://example.com" and "*" = all
+//    domains, for testing purposes only.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+      builder =>
+      {
+          builder.WithOrigins(
+            "http://example.com", "*");
+      });
+});
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizza API V1");
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizzas API V1");
 });
 
-app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
 
-app.MapPost("/pizzas", async (PizzaDb db, Pizza pizza) =>
-{
-  await db.Pizzas.AddAsync(pizza);
-  await db.SaveChangesAsync();
-  return Results.Created($"/pizza/{pizza.Id}", pizza);
+// 3) use the capability
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapGet("/", (PizzaDb db) => new { ConnectionString = db.Database.GetConnectionString(), db.Database.ProviderName });
+
+app.MapGet("/pizzas", async(PizzaDb db) => await db.Pizzas.ToListAsync());
+
+app.MapPost("/pizzas", async(PizzaDb db, Pizza pizza) => {
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizzas/{pizza.Id}", pizza);
 });
 
 app.MapPut("/pizzas/{id}", async (PizzaDb db, Pizza updatePizza, int id) =>
